2c428cff8b0f0a7f319b2146546ce7cf2dda8783
minorsecond <minorsecond@gmail.com>
2014-08-19 12:26:01 -0500

    Added code to enable moving reovered individuals back to susceptible compartment. The "removed" compartment is broken and doesn't work (but the calculations are correct).

--- seir.pl
+++ seir.pl
@@ -11,7 +11,10 @@ use Storable qw(dclone);
 STDOUT->autoflush;
 
 my $in = 0;
-	
+my $vac = 0;
+my $recsus = 0;
+my $RECOVERY_PERIOD = 0;
+
 print "Enter number of individuals: ";
 chomp(my $NUM_IND = <STDIN>);
 exit 0 if ($NUM_IND eq "");
@@ -32,53 +35,70 @@ print "Enter disease virulence: ";
 chomp(my $INFECTIVITY = <STDIN>);
 exit 0 if ($INFECTIVITY eq "");
 
+print "Enter disease mortality rate: ";
+chomp(my $MORTALITY = <STDIN>);
+exit 0 if ($MORTALITY eq "");
+
 print "Enter disease incubation period: ";
 chomp(my $INCUB = <STDIN>);
 exit 0 if ($INCUB eq "");
 
-print "Enter number of vaccinations per day: ";
-chomp(my $VAC = <STDIN>);
+#print "Enter number of vaccinations per day: ";
+#chomp(my $VAC = <STDIN>);
 
-print "Enter vaccine efficacy: ";
-chomp(my $EF = <STDIN>);
+#print "Enter vaccine efficacy: ";
+#chomp(my $EF = <STDIN>);
 
 print "Enter duration of model: ";
 chomp(my $DURATION = <STDIN>);
 exit 0 if ($DURATION eq "");
+
+print "Do recovered individuals become susceptible again? \n";
+$_ = <>;
+$recsus = 1 if /^Y/i;
+print "\n ";
+
+if ($recsus == 1)
+{
+	print "Enter recovery period: \n";
+	chomp(my $RECOVERY_PERIOD = <STDIN>);
+	}
 
 my $R0 = $CONTACT_RATE * $INFECTIVITY * $INFECTIOUS_PERIOD;
 my $V0 = (1 - 1 / $R0) * $NUM_IND;
 print "\n***Basic Reproductive Rate (R0): $R0.***\n";
 print "***Number to vaccinate to prevent sustained spread: $V0.***\n";
 
-print "Use this number in vaccination calculation? (Y/n)\n";
-$_ = <>;
-$VAC = $V0 / $DURATION if /^Y/i;
-print "\n ";
+#print "Use this number in vaccination calculation? (Y/n)\n";
+#$_ = <>;
+#$VAC = $V0 / $DURATION if /^Y/i;
+#print "\n ";
 
 {
 	local( $| ) = ( 1 );
 	print "Press <enter> to continue. ";
 	my $resp = <STDIN>;
 }
-
+my $person = ();
 my %population = ();
 
 #generate population by adding elements to population structure
 for(my $i = 0; $i< $NUM_IND; $i++) 
 {
 	$population{$i}{'infState'} = 0;
-	$population{$i}{'age'} = int(rand(80)); #unused at the moment.
+#	$population{$i}{'age'} = int(rand(80)); #unused at the moment.
 	$population{$i}{'dayOfInf'} = 0;
 	$population{$i}{'dayofExp'} = 0;
 	$population{$i}{'vacState'} = 0;
+	$population{$i}{'dayofRec'} = 0;
+	$population{$i}{'recState'} = 0;
 }
 
-#Expose a few individuals to start off the epidemic
+# Infect a few individuals to start off the epidemic
 for my $i (0 .. $INIT-1)
 {
 	$in = int(rand($NUM_IND));
-	$population{$in}{'infState'} = 1;
+	$population{$in}{'infState'} = 2;
 }
 
 #print S,I,R before start of simulation. s=0, e=1, i=2, r=3
@@ -86,6 +106,7 @@ my $sus = 0;
 my $exp = 0;
 my $inf = 0;
 my $rec = 0;
+my $dec = 0;
 foreach my $person (keys %population) 
 {
 	if($population{$person}{'infState'} == 0) {
@@ -100,11 +121,18 @@ foreach my $person (keys %population)
 	if($population{$person}{'infState'} == 3) {
 		$rec++;
 	}
+	if($population{$person}{'infState'} == 4)
+	{
+		$dec++;
+	}
+	if($population{$person}{'vacState'} == 1) {
+		$vac++;
+	}
 }
 
 
 print "Before initiation\n";
-print "SUS: ".$sus."\tEXP: ".$exp."\tINF: ".$inf."\tREM: ".$rec."\n";
+print "SUS: ".$sus."\tEXP: ".$exp."\tINF: ".$inf."\tREM: ".$rec."\tDEC: ".$dec."\tVAC: ".$vac."\n";
 
 #generating contacts
 #create a clone of %population
@@ -117,38 +145,6 @@ for(my $day = 0; $day < $DURATION; $day++)
 {
 	#Creating a temporary copy of population to make updates to infection state
 	my %population_copy = %{dclone(\%population)};
-
-	#Generate contacts for each exposed person
-	foreach my $person (keys %population) 
-	{
-		#for every exposed person make CR contacts in the clone
-		#update stats of the contacted person 
-		if($population{$person}{'infState'} == 1) 
-		{
-			for my $i (0 .. $CONTACT_RATE-1)
-			{
-				#Making sure that a person doesnt contact himself
-				my $r = $person;
-				while($r == $person) 
-				{
-					$r = int(rand($NUM_IND));
-				}
-				#Expose person
-				if(rand() < $INFECTIVITY)
-				{
-					if($population_copy{$r}{'infState'} == 0)
-					{
-						if($population_copy{$r}{'vacState'} == 0)
-						{
-						$population_copy{$r}{'infState'} = 1;
-						}
-					}
-				}	
-			}			
-		
-		}
-	
-	}
 
 	#Generate contacts for each infected person
 	foreach my $person (keys %population) 
@@ -181,32 +177,26 @@ for(my $day = 0; $day < $DURATION; $day++)
 		}
 	}
 
-	# Vaccinate individuals
-	foreach my $person (keys %population)
-	{
-		if($population{$person}{'infState'} == 0)
-		{
-			if($population{$person}{'vacState'} == 0)
-			{
-				for my $i (0 .. $VAC-1)
-				{
-					my $r = $person;
-					while($r == $person)
-					{
-						$r = int(rand($NUM_IND));
-					}
-					if(rand() < $EF)
-					{
-						if($population_copy{$r}{'vacState'} == 0)
-						{
-						$population_copy{$r}{'vacState'} = 1;
-						}
-					}
-				}
-			}
-		}
-	}
-	
+# Vaccinate individuals
+#	for my $i (0 .. $VAC - 1)
+#	{
+#	keys %population;
+#			if($population{$person}{'vacState'} == 0)
+#				{
+#					my $r = $person;
+#					while($r == $person)
+#						$r = int(rand($NUM_IND));
+#				}
+#						if(rand() < $EF)
+#						{
+#							if($population_copy{$person}{'vacState'} == 0)
+#							{
+#								$population_copy{$person}{'vacState'} = 1;
+#							}
+#						}
+#				}
+#			}
+
 	#copy contents of temporary copy to original one
 	%population = %{dclone(\%population_copy)};
 
@@ -230,6 +220,34 @@ for(my $day = 0; $day < $DURATION; $day++)
 				$population{$person}{'infState'} = 3;
 			}
 		}
+		
+		if ($recsus == 1)
+		{
+			if($population{$person}{'recState'} == 0)
+			{
+				if(rand() > $MORTALITY)
+				{
+					if($population{$person}{'infState'} == 3)
+					{
+						$population{$person}{'recState'} = 1;
+					}
+				}
+			
+				elsif($population{$person}{'infState'} == 3)
+				{
+					$population{$person}{'infState'} = 4;
+				}
+			}
+			
+			if($population{$person}{'recState'} == 1)
+			{
+				$population{$person}{'dayofRec'}++;
+				if($population{$person}{'dayofRec'} >= $RECOVERY_PERIOD)
+				{
+					$population{$person}{'infState'} = 0;
+				}
+			}
+		}
 	}
 
 
@@ -238,6 +256,8 @@ for(my $day = 0; $day < $DURATION; $day++)
 	my $exp = 0;
 	my $inf = 0;
 	my $rec = 0;
+	my $dec = 0;
+	my $vac = 0;
 	foreach my $person (keys %population) 
 	{
 		if($population{$person}{'infState'} == 0) 
@@ -256,11 +276,19 @@ for(my $day = 0; $day < $DURATION; $day++)
 		{
 			$rec++;
 		}
+		if($population{$person}{'infState'} == 4)
+		{
+			$dec++;
+		}
+		if($population{$person}{'vacState'} == 1)
+		{
+			$vac++;
+		}
 	}
 
 
 	print "Day ".$day."\n";
-	print "SUS: ".$sus."\tEXP: ".$exp."\tINF: ".$inf."\tREM: ".$rec."\n";
+	print "SUS: ".$sus."\tEXP: ".$exp."\tINF: ".$inf."\tREM: ".$rec."\tDEC: ".$dec."\tVAC: ".$vac."\n";
 
 	#print Dumper(\%population);
 	#print "\n-------------\n";
